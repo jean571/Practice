ea01a0bde2d26eab23235e43e02c3d04c813bb50
Jean Gonzalez <jeangonzalez@vassar.edu>
2021-04-16 19:23:54 -0400

    first commit

---
+++ app/src/main/java/com/example/practice/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,19 @@
+package com.example.practice.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("HI JOJO");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
